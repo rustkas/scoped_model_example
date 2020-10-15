91f076f9170f2c0c9ec8839483b5457082ce46a6
Anatolii <developer@languages-with-music.com>
2020-10-15 19:21:09 +0200

    Hide 'debug' badge
    Refresh button is hidden by 'debug' badge. So just hide badge and press to 'Refresh' button.

--- lib/app.dart
+++ lib/app.dart
@@ -4,6 +4,9 @@ import 'screens/location_list.dart';
 class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: LocationList());
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: LocationList()
+    );
   }
 }
